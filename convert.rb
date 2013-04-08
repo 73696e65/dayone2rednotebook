8d6be87e260f30c59caba8de3677ba774440756d
Norbert Szetei <sine@trip.sk>
2013-04-08 19:46:46 +0200

    added output directory to ignore

--- convert.rb
+++ convert.rb
@@ -4,4 +4,11 @@
 # Note: works without photos
 # Version 0.01
 
+dayone_dir = ARGV[0]
+output_dir = ARGV[1]
+
+unless ARGV[0] and ARGV[1]
+  puts "syntax: #{$0} <dayone_import_dir> <rednotebook_output_dir>" ; exit
+end
+
 
